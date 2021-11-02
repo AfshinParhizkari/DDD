a76f9e4e95a0fe8203b34bd2a78955ae83948824
AfshinParhizkari <Afshin.Parhizkari@gmail.com>
2021-11-02 05:01:11 +0330

    Product Service is added to service bus

---
+++ Product/src/main/java/com/afshin/product/infrastructure/repository/ProductDao.java
@@ -0,0 +1,21 @@
+package com.afshin.product.infrastructure.repository;
+/**
+ * @Project DDD
+ * @Author Afshin Parhizkari
+ * @Date Apr 26, 2021 2:58:19 AM
+ * @version
+ * Created by Eclipse 2020-09
+ * Email:       Afshin.Parhizkari@gmail.com
+ * Description: 
+*/
+
+import com.afshin.product.domain.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductDao extends JpaRepository<Product, Integer>{
+	Page<Product> findAll(Pageable pageable);
+	List<Product> findByProductpk(Integer productcode);
+}
